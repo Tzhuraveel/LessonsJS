0b341ddb7cba2ff7f01368623ef0d58fda4bf2fd
Tzhuraveel <timofiyzhuravel2003@gmail.com>
2022-11-01 11:58:06 +0200

    error correction

---
+++ homework1/main.js
@@ -0,0 +1,86 @@
+let s = 'hello';
+console.log(s);
+
+let s1 = 'owu';
+console.log(s1);
+
+let s2 = 'com';
+console.log(s2);
+
+let s3 = 'ua';
+console.log(s3);
+
+/*------------*/
+
+let number = 1;
+console.log(number);
+
+let number1 = 10;
+console.log(number1);
+
+let number2 = -999;
+console.log(number2);
+
+let number3 = 123;
+console.log(number3);
+
+const PI = 3.14;
+console.log(PI);
+
+let number5 = 2.7;
+console.log(number5);
+
+let number6 = 16;
+console.log(number6);
+
+let bool = true;
+console.log(bool);
+
+let bool1 = false;
+console.log(bool1);
+
+/*-------------*/
+
+let firstName = 'Zhuravel';
+let middleName = 'Timofii';
+let lastName = 'Valeriyovych';
+let person = `${firstName} ${middleName} ${lastName}`;
+console.log(person);
+
+/*------------*/
+
+let a = 100;
+console.log(typeof a);
+
+let b = '100';
+console.log(typeof b);
+
+let c = true;
+console.log(typeof c);
+
+/*----------------*/
+
+let name1 = prompt('Are you sure ' + firstName);
+let name2 = prompt('Are you absolute sure ' + middleName);
+let name3 = prompt('Are you trust me ' + lastName);
+console.log(name1, name2, name3);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
